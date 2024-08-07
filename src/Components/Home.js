65db7f6fbd2a5f14f854bba8c8e591a93db13356
Your Name <isaacchukwuka67@gmail.com>
2024-08-07 19:14:51 +0100

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,176 @@
+import React, { useState, useEffect } from "react";
+import { Card, CardBody, Col, Container, Row, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import NavBar from "./Nav";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import imgpattern from "../Images/homeimage.svg";
+import soladodge from "../Images/bettersoldodge.png";
+
+const Home = () => {
+  const [animate, setAnimate] = useState(false);
+  useEffect(() => {
+    setAnimate(true);
+  }, []);
+  const gradientBackgroundStyle = {
+    background: "linear-gradient(360deg,  #FFA07A 10%, #FCFCFC 100%)",
+  };
+  return (
+    <React.Fragment>
+      <section
+        className="section pb-0 hero-section"
+        id="home"
+        style={gradientBackgroundStyle}
+      >
+        <div className="bg-overlay bg-overlay-pattern"></div>
+        <Container>
+          <Row className="justify-content-center">
+            <Col xs={12} sm={10} lg={8}>
+              <div className="text-center text-light mt-4 mt-lg-5 pt-5">
+                {/* <div
+                  className="btn rounded-pill get-started-btn mb-3 mb-sm-0"
+                  style={{ backgroundColor: "#B22222", color: "white" }}
+                >
+                  Get Started
+                  <i className="ri-arrow-right-line align-middle ms-1"></i>
+                </div> */}
+                <span className="small-text w-0 text-dark mt-2 mt-sm-0 ms-sm-3">
+                  woo-o-oof woo-o-oof The dog has finally been let out.
+                </span>
+
+                <div
+                  className={`d-flex flex-column align-items-center justify-content-center mb-4 ${
+                    animate ? "animate-slide-in" : ""
+                  }`}
+                >
+                  <h1
+                    className="display-2 fw-bold mb-3 lh-base custom-title"
+                    style={{
+                      fontFamily: "Poppins, sans-serif",
+                      color: "#B22222",
+                    }}
+                  >
+                    SOLADOGE
+                  </h1>
+                  <img
+                    src={soladodge}
+                    alt="SOLADOGE"
+                    className="soladoge-image mb-3"
+                  />
+                </div>
+                <p
+                  className="lead text-break text-dark custom-lead"
+                  style={{ fontFamily: "Mulish, sans-serif" }}
+                >
+                  more than just a "meme" It's a movement....
+                </p>
+                <div className="d-flex justify-content-center mt-3 mb-5">
+                  <Button
+                    className="btn mb-3 btn-primary"
+                    style={{ background: "#101010" }}
+                    onClick={() =>
+                      document
+                        .getElementById("submit")
+                        .scrollIntoView({ behavior: "smooth" })
+                    }
+                  >
+                    Join Waitlist{" "}
+                    <i className="ri-arrow-right-line align-middle ms-1"></i>
+                  </Button>
+                </div>
+                <div className="glassmorphism-container">
+                  <div className="glassmorphism-item">
+                    <h2>690k+</h2>
+                    <p>Holders</p>
+                  </div>
+                  <div className="glassmorphism-item">
+                    <h2>191</h2>
+                    <p>Interactions</p>
+                  </div>
+                  <div className="glassmorphism-item">
+                    <h2>27</h2>
+                    <p>Chains</p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <style jsx>{`
+        @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Mulish&display=swap");
+
+        @keyframes slideIn {
+          from {
+            transform: translateY(20px);
+            opacity: 0;
+          }
+          to {
+            transform: translateY(0);
+            opacity: 1;
+          }
+        }
+        .animate-slide-in {
+          animation: slideIn 0.8s ease-out forwards;
+        }
+        .custom-title {
+          font-size: 4rem;
+          line-height: 1.2;
+        }
+        .soladoge-image {
+          width: 80%;
+          max-width: 300px;
+          height: auto;
+          margin-bottom: 1rem;
+        }
+        .custom-lead {
+          font-size: 1.2rem;
+        }
+        .glassmorphism-container {
+          display: flex;
+          justify-content: space-around;
+          margin-top: 2rem;
+        }
+        .glassmorphism-item {
+          background: rgba(255, 255, 255, 0.2);
+          backdrop-filter: blur(10px);
+          border-radius: 10px;
+          padding: 1rem;
+          text-align: center;
+          color: #333;
+        }
+        .glassmorphism-item h2 {
+          font-size: 2rem;
+          margin-bottom: 0.5rem;
+          color: #b22222;
+        }
+        .glassmorphism-item p {
+          font-size: 1rem;
+          margin-bottom: 0;
+        }
+        @media (max-width: 576px) {
+          .custom-title {
+            font-size: 3rem;
+          }
+          .soladoge-image {
+            width: 100%;
+            max-width: 250px;
+          }
+          .custom-lead {
+            font-size: 1rem;
+          }
+          .glassmorphism-container {
+            flex-direction: column;
+            align-items: center;
+          }
+          .glassmorphism-item {
+            margin-bottom: 1rem;
+            width: 80%;
+          }
+        }
+      `}</style>
+    </React.Fragment>
+  );
+};
+export default Home;
---
+++ src/Components/Services.js
@@ -0,0 +1,246 @@
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Card,
+  Col,
+  Row,
+  CardBody,
+  Button,
+  Fade,
+} from "react-bootstrap";
+import NavBar from "./Nav";
+
+import img1 from "../Images/solanacoin.jpg";
+import img2 from "../Images/soladodgeimage.jpg";
+import img3 from "../Images/dodgelogo.png";
+import img4 from "../Images/soladodgeimage.jpg";
+import img6 from "../Images/teacher.svg";
+import img5 from "../Images/curriculumpic.png";
+import bcg from "../Images/dodgebcg.jpg";
+
+const Portfolio = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(true);
+  }, []);
+
+  const sectionStyle = {
+    backgroundImage: `url(${bcg})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    position: "relative",
+  };
+
+  const overlayStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(255, 255, 255, 0.9)", // Very opaque white
+    zIndex: 1,
+  };
+
+  const contentStyle = {
+    position: "relative",
+    zIndex: 2,
+  };
+
+  const cardStyle = {
+    background: "rgba(253, 249, 239, 0.8)", // Slightly transparent
+    transition: "all 0.3s ease-in-out",
+  };
+
+  const buttonStyle = {
+    background: "#FAF3DD",
+    border: "none",
+    transition: "background-color 0.3s ease-in-out",
+    color: "#5C0632",
+  };
+
+  const handleCardHover = (e, enter) => {
+    e.currentTarget.style.transform = enter
+      ? "translateY(-5px)"
+      : "translateY(0)";
+    e.currentTarget.style.boxShadow = enter
+      ? "0 10px 20px rgba(0, 0, 0, 0.1)"
+      : "none";
+  };
+
+  return (
+    <React.Fragment>
+      <section
+        id="portfolio"
+        className="position-relative"
+        style={sectionStyle}
+      >
+        <div style={overlayStyle}></div>
+        <Container style={contentStyle}>
+          <Row className="justify-content-center mt-3">
+            <Col lg={8}>
+              <div className="text-center pt-5 text-dark mb-5">
+                <h1
+                  className="fw-bold"
+                  style={{
+                    fontFamily: "Poppins, sans-serif",
+                    color: "#B22222",
+                  }}
+                >
+                  ABOUT SOLADODGE
+                </h1>
+              </div>
+            </Col>
+          </Row>
+          <Row className="justify-content-center gy-4">
+            {/* Card 1 */}
+            <Col md={3}>
+              <Fade in={open}>
+                <Card
+                  className="rounded-5 card-animate h-100"
+                  style={cardStyle}
+                  onMouseEnter={(e) => handleCardHover(e, true)}
+                  onMouseLeave={(e) => handleCardHover(e, false)}
+                >
+                  <CardBody className="d-flex flex-column">
+                    <div className="p-3 text-center">
+                      <div className="mt-auto">
+                        <img
+                          src={img1}
+                          alt=""
+                          height="55"
+                          className="mb-3 pb-2"
+                        />
+                      </div>
+                      <Button className="rounded-pill" style={buttonStyle}>
+                        Solana Ecosystem
+                      </Button>
+                      <p
+                        className="card-text mb-1 text-center text-muted"
+                        style={{ fontFamily: "Poppins, sans-serif" }}
+                      >
+                        SolaDoge is the meme token that is taking the Solana
+                        Ecosystem by Storm.
+                      </p>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Fade>
+            </Col>
+
+            {/* Card 2 */}
+            <Col md={3}>
+              <Fade in={open}>
+                <Card
+                  className="rounded-5 card-animate h-100"
+                  style={cardStyle}
+                  onMouseEnter={(e) => handleCardHover(e, true)}
+                  onMouseLeave={(e) => handleCardHover(e, false)}
+                >
+                  <CardBody className="d-flex flex-column">
+                    <div className="p-3 text-center">
+                      <div className="mt-auto">
+                        <img
+                          src={img4}
+                          alt=""
+                          height="55"
+                          className="mb-3 pb-2"
+                        />
+                      </div>
+                      <Button className="rounded-pill" style={buttonStyle}>
+                        Global Access
+                      </Button>
+                      <p
+                        className="card-text mt-2 mb-1 text-center text-muted"
+                        style={{ fontFamily: "Poppins, sans-serif" }}
+                      >
+                        DEEP INTEGRATIONS AS A UTILITY TOKEN ACROSS A WIDE BASE
+                        OF APPLICATIONS AND PROTOCOLS WITHIN THE WEB3 ECOSYSTEM
+                      </p>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Fade>
+            </Col>
+
+            {/* Card 3 */}
+            <Col md={3}>
+              <Fade in={open}>
+                <Card
+                  className="rounded-5 card-animate h-100"
+                  style={cardStyle}
+                  onMouseEnter={(e) => handleCardHover(e, true)}
+                  onMouseLeave={(e) => handleCardHover(e, false)}
+                >
+                  <CardBody className="d-flex flex-column">
+                    <div className="p-3 text-center">
+                      <div className="mt-auto">
+                        <img
+                          src={img3}
+                          alt=""
+                          height="55"
+                          className="mb-3 pb-2"
+                        />
+                      </div>
+                      <Button className="rounded-pill" style={buttonStyle}>
+                        origin
+                      </Button>
+                      <p
+                        className="card-text mb-1 text-center text-muted"
+                        style={{ fontFamily: "Poppins, sans-serif" }}
+                      >
+                        SolaDoge, a state of the art meme token on the Solana
+                        Blockchain is the latest addition to this trend.
+                      </p>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Fade>
+            </Col>
+
+            {/* Card 4 */}
+            <Col md={3}>
+              <Fade in={open}>
+                <Card
+                  className="rounded-5 card-animate h-100"
+                  style={cardStyle}
+                  onMouseEnter={(e) => handleCardHover(e, true)}
+                  onMouseLeave={(e) => handleCardHover(e, false)}
+                >
+                  <CardBody className="d-flex flex-column">
+                    <div className="p-3 text-center">
+                      <div className="mt-auto">
+                        <img
+                          src={img4}
+                          alt=""
+                          height="55"
+                          className="mb-3 pb-2"
+                        />
+                      </div>
+                      <Button className="rounded-pill" style={buttonStyle}>
+                        Membership
+                      </Button>
+                      <p
+                        className="card-text mb-1 text-center text-muted"
+                        style={{ fontFamily: "Poppins, sans-serif" }}
+                      >
+                        Network with high profile individuals like you and
+                        strike a deeper connection and get access to premium
+                        services like: unlimited private jet access, highly
+                        personalised travel experience, priority scheduling,
+                        empty leg prior notification, etc
+                      </p>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Fade>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Portfolio;
